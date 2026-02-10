c244f50a896690d063810c93c4939d95f8d82574
asksa1256 <asksa1256@gmail.com>
2026-02-10 23:37:37 +0900

    chore: zustand, shadcn/ui 패키지 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@/shared/ui/button/button';
 import './styles/globals.css';
 
 function App() {
   return (
-    <div className='flex w-full items-center justify-center'>Weather App</div>
+    <div className='flex w-full items-center justify-center'>
+      Weather App <Button>테스트</Button>
+    </div>
   );
 }
 
