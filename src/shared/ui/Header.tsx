d361b9c7b1ad08c4022e762b8cf5dd500752c7d4
asksa1256@gmail.com <asksa1256@gmail.com>
2026-02-13 22:55:39 +0900

    feat: 로고에 메인 링크 연결

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -11,6 +11,7 @@ import FavoriteList from '@/widgets/favorites/ui/FavoriteList';
 import { Menu, Star } from 'lucide-react';
 import { useState } from 'react';
 import { Button } from './button/button';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
@@ -19,12 +20,12 @@ const Header = () => {
     <header className='sticky top-0 z-50 w-full border-b bg-white/80 backdrop-blur-md'>
       <div className='mx-auto flex h-16 items-center justify-between px-4 md:max-w-[600px] md:px-0'>
         {/* 로고 */}
-        <div className='flex items-center gap-2'>
+        <Link to='/' className='flex items-center gap-2' title='리얼캐스트 메인으로 이동'>
           <img src={LogoImg} alt='로고 이미지' className='w-8' />
           <h1 className='text-xl font-bold tracking-tight text-gray-900'>
             RealCast
           </h1>
-        </div>
+        </Link>
 
         {/* 즐겨찾기 사이드바(Drawer) */}
         <Drawer
