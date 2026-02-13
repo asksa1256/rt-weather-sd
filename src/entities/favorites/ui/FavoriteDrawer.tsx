5e4b8ba0e235a15526746a7bd923aa2a1a86810a
asksa1256@gmail.com <asksa1256@gmail.com>
2026-02-13 23:17:20 +0900

    refactor: Header에서 즐겨찾기 사이드바 분리

---
+++ src/entities/favorites/ui/FavoriteDrawer.tsx
@@ -0,0 +1,53 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger
+} from '@/shared/ui/drawer';
+import { Button } from '@/shared/ui/button/button';
+import FavoriteList from '@/widgets/favorites/ui/FavoriteList';
+import { Menu, Star, X } from 'lucide-react';
+import { useState } from 'react';
+
+const FavoriteDrawer = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  return (
+    <Drawer
+      direction='right'
+      open={isDrawerOpen}
+      onOpenChange={setIsDrawerOpen}
+    >
+      <DrawerTrigger asChild>
+        <Button variant='ghost' className='relative'>
+          <Menu className='size-5' />
+          <span>즐겨찾기</span>
+        </Button>
+      </DrawerTrigger>
+
+      <DrawerContent className='top-0 right-0 left-auto mt-0 h-screen w-full rounded-none sm:w-[400px]'>
+        <div className='mx-auto flex h-full w-full flex-col'>
+          <DrawerHeader className='border-b px-6 py-4'>
+            <div className='flex items-center justify-between'>
+              <DrawerTitle className='flex items-center gap-2 text-2xl font-bold'>
+                <Star className='size-5 fill-yellow-500 text-yellow-500' />
+                즐겨찾기
+              </DrawerTitle>
+              <DrawerClose asChild>
+                <Button variant='ghost' size='icon' title='즐겨찾기 닫기'><X className='size-5' /></Button>
+              </DrawerClose>
+            </div>
+          </DrawerHeader>
+
+          <div className='flex-1 overflow-y-auto p-6'>
+            <FavoriteList onCloseDrawer={() => setIsDrawerOpen(false)} />
+          </div>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+export default FavoriteDrawer;
--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -1,21 +1,8 @@
 import LogoImg from '@/shared/assets/images/logo.png';
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from '@/shared/ui/drawer';
-import FavoriteList from '@/widgets/favorites/ui/FavoriteList';
-import { Menu, Star } from 'lucide-react';
-import { useState } from 'react';
-import { Button } from './button/button';
 import { Link } from 'react-router-dom';
+import FavoriteDrawer from '@/entities/favorites/ui/FavoriteDrawer';
 
 const Header = () => {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-
   return (
     <header className='sticky top-0 z-50 w-full border-b bg-white/80 backdrop-blur-md'>
       <div className='mx-auto flex h-16 items-center justify-between px-4 md:max-w-[600px] md:px-0'>
@@ -27,39 +14,8 @@ const Header = () => {
           </h1>
         </Link>
 
-        {/* 즐겨찾기 사이드바(Drawer) */}
-        <Drawer
-          direction='right'
-          open={isDrawerOpen}
-          onOpenChange={setIsDrawerOpen}
-        >
-          <DrawerTrigger asChild>
-            <Button variant='ghost' className='relative'>
-              <Menu className='size-5' />
-              <span>즐겨찾기</span>
-            </Button>
-          </DrawerTrigger>
-
-          <DrawerContent className='top-0 right-0 left-auto mt-0 h-screen w-full rounded-none sm:w-[400px]'>
-            <div className='mx-auto flex h-full w-full flex-col'>
-              <DrawerHeader className='border-b px-6 py-4'>
-                <div className='flex items-center justify-between'>
-                  <DrawerTitle className='flex items-center gap-2 text-2xl font-bold'>
-                    <Star className='size-5 fill-yellow-500 text-yellow-500' />
-                    즐겨찾기
-                  </DrawerTitle>
-                  <DrawerClose asChild>
-                    <Button variant='ghost'>닫기</Button>
-                  </DrawerClose>
-                </div>
-              </DrawerHeader>
-
-              <div className='flex-1 overflow-y-auto p-6'>
-                <FavoriteList onCloseDrawer={() => setIsDrawerOpen(false)} />
-              </div>
-            </div>
-          </DrawerContent>
-        </Drawer>
+        {/* 즐겨찾기 사이드바 */}
+        <FavoriteDrawer />
       </div>
     </header>
   );
